c246daa21d566932a80f7527998e28b31327964a
Qnemes <qnemes@gmail.com>
2020-03-18 21:16:31 +0100

    initial commit

---
+++ info.js
@@ -0,0 +1,50 @@
+let info = JSON.parse(localStorage.getItem('company-info'));
+let average = (info.summaryIncome/info.incomes.length).toFixed(2);
+let ctx = document.getElementById('myChart').getContext('2d');
+
+// sorting date of company incomes that passed from previous script by localStorage
+info.incomes.sort(function(a, b) {
+    return new Date(b.date)-new Date(a.date);
+});
+document.getElementById('title').innerHTML+= info.name;
+document.getElementById('city').innerHTML+= info.city;
+document.getElementById('income').innerHTML+= info.summaryIncome; 
+document.getElementById('average').innerHTML+= average;
+
+// console.log(info.incomes[0].value);
+// dynamically pushing data to chart
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+// creating chart.js bar chart
+let myLineChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            label: 'Monthly incomes',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+        }]
+    }
+});
+// getting rid of useless time and characters while showing graph
+for (let i in info.incomes){
+    info.incomes[i].date = info.incomes[i].date.slice(0,10)
+    addData(myLineChart,info.incomes[i].date,info.incomes[i].value) 
+}
+var comparison =[];
+for (let i in info.incomes){    
+comparison.push(info.incomes[i].date.substr(0, 7));
+}
+var lastmonth = 0;
+for (let i in comparison){
+    if (comparison[0] === comparison[i]){        
+        lastmonth+=parseFloat(info.incomes[i].value);
+        console.log(lastmonth);
+    }      
+}
+document.getElementById('lastmonth').innerHTML+= lastmonth;
---
+++ main.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {  
+const responses = [];
+
+for (let i = 1; i <= 300; i++) {
+  //I should use company id instead of i, but it was too late when I just realised it
+  responses.push(fetch(`https://recruitment.hal.skygate.io/incomes/${i}`).then(response => {    
+    if (response.ok) return response.json();
+    throw new Error(response.statusText);
+}));
+}
+Promise.all(responses)
+    .then(data => {   
+      //array for easy future data manipulating
+        mergedArr = [];
+        summaryArr = [];        
+        // accumulating and creating new object property that contains sum of company incomes 
+        summaryArr = data.map(item => ({   
+          ...item,  
+          summaryIncome: item.incomes.reduce((acc,income) => 
+            acc+= parseFloat(income.value), 0).toFixed(2)      
+        }));          
+        return fetch("https://recruitment.hal.skygate.io/companies") 
+    .then(response => {
+        if (response.ok) return response.json();
+        throw new Error(response.statusText);
+    });
+  })
+    .then(data => {       
+      // merging main data with summaryIncome property
+        for (let i in data){
+        mergedArr.push({...data[i],...summaryArr[i]})  
+        }
+        // creating table with descending order by 3 cell (total income)
+        let table = $('#example').DataTable({
+            select: true,
+            order: [3,'desc'],
+            data: mergedArr,
+            columns: [
+                { data: 'id' },
+                { data: 'name' },
+                { data: 'city' },
+                { data: 'summaryIncome'}
+          ]
+        });  
+        //redirecting to another html page by selecting row and passing all data from the row
+        table.on('select', function (e, dt, type, indexes) {
+            let data = table.rows( indexes ).data().pluck( 'id','name');
+            let objectString = JSON.stringify(mergedArr[data[0]-1]);  
+            localStorage.setItem('company-info', objectString);  
+            window.open('/info.html');             
+        });
+        //showing table headers only when data fully get
+        $("div#extraControls").removeClass("hidden");      
+})
+    //standard error handler
+    .catch(error => {
+        console.log("Error" + error);       
+});
+});
